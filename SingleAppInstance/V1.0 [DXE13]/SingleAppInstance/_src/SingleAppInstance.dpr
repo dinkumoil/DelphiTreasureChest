b3947dd6ff04b0e28ec756d49cd4000a8f334ad8
Andreas Heim <dinkumoil@users.noreply.github.com>
2024-03-09 13:30:39 +0100

    SingleAppInstance
    - fixed: Code file contains wrong license

--- SingleAppInstance/V1.0 [DXE13]/SingleAppInstance/_src/SingleAppInstance.dpr
+++ SingleAppInstance/V1.0 [DXE13]/SingleAppInstance/_src/SingleAppInstance.dpr
@@ -8,12 +8,18 @@
 // Author   : Andreas Heim, 2020-06
 //
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the Mozilla Public License Version 2.0.
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License version 3 as published
+// by the Free Software Foundation.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 { **************************************************************************** }
 
